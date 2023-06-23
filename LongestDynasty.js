d9eb5be668d07905003def39bd0ef938dcc44cd6
Khim1111 <124416784+Khim1111@users.noreply.github.com>
2023-06-23 20:47:28 +0800

    Fix bug

--- LongestDynasty.js
+++ LongestDynasty.js
@@ -1,63 +1,63 @@
-function longestDynasty(dynastyReign) {
-    if (dynastyReign.length === 0) {
-      return "No Data";
-    }
-  
-    let longest = '';
-    let longestDuration = 0;
-  
-    for (let i = 0; i < dynastyReign.length; i++) {
-      const dynasty = Object.keys(dynastyReign[i])[0];
-      const year = dynastyReign[i][dynasty];
-  
-      const startYear = convertYear(year);
-      const endYear = (i === dynastyReign.length - 1) ? Infinity : convertYear(dynastyReign[i + 1][Object.keys(dynastyReign[i + 1])[0]]);
-  
-      if (startYear !== 'Invalid' && endYear !== 'Invalid') {
-        const duration = endYear - startYear;
-        if (duration > longestDuration) {
-          longestDuration = duration;
-          longest = dynasty;
-        }
-      }
-    }
-  
-    return longest;
-  }
-  
-  function convertYear(year) {
-    const romanNumerals = { 'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1 };
-    let result = 0;
-  
-    for (let i = 0; i < year.length; i++) {
-      const current = romanNumerals[year[i]];
-      const next = (i + 1 < year.length) ? romanNumerals[year[i + 1]] : 0;
-  
-      if (current === undefined) {
-        return 'Invalid';
-      }
-  
-      if (current < next) {
-        result -= current;
-      } else {
-        result += current;
-      }
-    }
-  
-    return result;
-  }
-  
- 
-  var dynastyReign = [
-    { "San Dynasty": "MXLI" },
-    { "Viloria Dynasty": "MCCCIIII" },
-    { "Tan Dynasty": "MCCCXCVIII" },
-    { "Bon Dynasty": "MCDXLV" },
-    { "Maiko Dynasty": "MDCLXIV" },
-    { "Paul Dynasty": "MCMXLIX" },
-    { "Andre Dynasty": "MMMXICX" }
+var dynastyReign = [
+    { 'San Dynasty': 'MXLI' },
+    { 'Viloria Dynasty': 'MCCCIIII' },
+    { 'Tan Dynasty': 'MCCCXCVIII' },
+    { 'Bon Dynasty': 'MCDXLV' },
+    { 'Maiko Dynasty': 'MDCLXIV' },
+    { 'Paul Dynasty': 'MCMXLIX' },
+    { 'Andre Dynasty': 'MMMXICX' },
   ];
   
-  var longestDynasty = longestDynasty(dynastyReign);
-  document.getElementById("result").textContent = "Longest Reigning Dynasty: " + longestDynasty;
+  function longestDynasty (dynastyReign) {
+    if (dynastyReign.length < 1) return 'No Data';
   
+    const filtered = dynastyReign
+      .filter(y => convertYear(Object.values(y).toString()) !== null)
+    const fValues = filtered
+      .map(el => convertYear(Object.values(el).toString()))
+  
+    let reign = 0;
+    let max = 0;
+    let temp = 0;
+    for (let i = 1; i < fValues.length; i++) {
+      temp = Math.abs(fValues[i] - fValues[i - 1])
+      if (temp > max) {
+        max = temp
+        reign = i
+      }
+    }
+    return filtered[reign];
+  
+  }
+  
+  function convertYear (year) {
+    let y = 0;
+    if (typeof y === 'undefined' || y === null) return y;
+  
+    let regex = new RegExp('^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$');
+    if (!regex.test(year)) return null;
+  
+    let romanMap = new Map();
+    romanMap.set('I', 1);
+    romanMap.set('V', 5);
+    romanMap.set('X', 10);
+    romanMap.set('L', 50);
+    romanMap.set('C', 100);
+    romanMap.set('D', 500);
+    romanMap.set('M', 1000);
+  
+    let l = year.length;
+    for (var i = 0; i < l; i++) {
+      if (romanMap.get(year.charAt(i)) < romanMap.get(year.charAt(i + 1))) {
+        y -= romanMap.get(year.charAt(i))
+      } else {
+        y += romanMap.get(year.charAt(i))
+      }
+    }
+  
+    return y;
+  }
+  
+  console.log(longestDynasty(dynastyReign));
+
+  document.write(JSON.stringify(longestDynasty(dynastyReign)));
